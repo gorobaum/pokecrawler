81d563fb82342df9128d25f8f6164954ac2b2d8c
Thiago Nunes <gorobaum@gmail.com>
2014-02-25 17:22:48 -0300

    Using HtmlXPathSelector

--- pokecrawler/pokecrawler/spiders/pokemondb_spider.py
+++ pokecrawler/pokecrawler/spiders/pokemondb_spider.py
@@ -1,3 +1,5 @@
+from scrapy.spider import BaseSpider
+from scrapy.selector import HtmlXPathSelector
 from scrapy.spider import Spider
 from scrapy.selector import Selector
 
@@ -12,11 +14,11 @@ class PokemonDBSpider(Spider):
 
     def parse(self, response):
     	pokemon = PokecrawlerItem()
-    	sel = Selector(response)
-    	
-    	pokemon['name'] = sel.xpath('//h1/text()')[0].extract()
+    	hxs = HtmlXPathSelector(response)
 
-    	types = sel.xpath('//tr[th/text() = "Type"]/td/a/text()').extract()
+    	pokemon['name'] = hxs.select('//h1/text()')[0].extract()
+
+    	types = hxs.select('//tr[th/text() = "Type"]/td/a/text()').extract()
     	if len(types) > 1:
     		pokemon['type1'] = types[0]
     		pokemon['type2'] = types[1]
@@ -24,11 +26,11 @@ class PokemonDBSpider(Spider):
     		pokemon['type1'] = types[0]
     		pokemon['type2'] = "None"
 
-    	pokemon['hp'] = sel.xpath('//tr[th/text() = "HP"]/td/text()')[0].extract()
-    	pokemon['attack'] = sel.xpath('//tr[th/text() = "Attack"]/td/text()')[0].extract()
-    	pokemon['defense'] = sel.xpath('//tr[th/text() = "Defense"]/td/text()')[0].extract()
-    	pokemon['spAttack'] = sel.xpath('//tr[th/text() = "Sp. Atk"]/td/text()')[0].extract()
-    	pokemon['spDefense'] = sel.xpath('//tr[th/text() = "Sp. Def"]/td/text()')[0].extract()
-    	pokemon['speed'] = sel.xpath('//tr[th/text() = "Speed"]/td/text()')[0].extract()
-    	pokemon['total'] = sel.xpath('//tr[th/text() = "Total"]/td/b/text()')[0].extract()
+    	pokemon['hp'] = hxs.select('//tr[th/text() = "HP"]/td/text()')[0].extract()
+    	pokemon['attack'] = hxs.select('//tr[th/text() = "Attack"]/td/text()')[0].extract()
+    	pokemon['defense'] = hxs.select('//tr[th/text() = "Defense"]/td/text()')[0].extract()
+    	pokemon['spAttack'] = hxs.select('//tr[th/text() = "Sp. Atk"]/td/text()')[0].extract()
+    	pokemon['spDefense'] = hxs.select('//tr[th/text() = "Sp. Def"]/td/text()')[0].extract()
+    	pokemon['speed'] = hxs.select('//tr[th/text() = "Speed"]/td/text()')[0].extract()
+    	pokemon['total'] = hxs.select('//tr[th/text() = "Total"]/td/b/text()')[0].extract()
     	return pokemon
