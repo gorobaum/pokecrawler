9dacac86a080eb806ddf3d6cc4534d154db73e09
Thiago Nunes <gorobaum@gmail.com>
2014-02-25 17:03:08 -0300

    Fixes

--- pokecrawler/pokecrawler/spiders/pokemondb_spider.py
+++ pokecrawler/pokecrawler/spiders/pokemondb_spider.py
@@ -13,6 +13,8 @@ class PokemonDBSpider(Spider):
     def parse(self, response):
     	pokemon = PokecrawlerItem()
     	sel = Selector(response)
+    	
+    	pokemon['name'] = sel.xpath('//h1/text()')[0].extract()
 
     	types = sel.xpath('//tr[th/text() = "Type"]/td/a/text()').extract()
     	if len(types) > 1:
@@ -22,12 +24,11 @@ class PokemonDBSpider(Spider):
     		pokemon['type1'] = types[0]
     		pokemon['type2'] = "None"
 
-    	pokemon['name'] = sel.xpath('//h1/text()').extract()
     	pokemon['hp'] = sel.xpath('//tr[th/text() = "HP"]/td/text()')[0].extract()
     	pokemon['attack'] = sel.xpath('//tr[th/text() = "Attack"]/td/text()')[0].extract()
     	pokemon['defense'] = sel.xpath('//tr[th/text() = "Defense"]/td/text()')[0].extract()
     	pokemon['spAttack'] = sel.xpath('//tr[th/text() = "Sp. Atk"]/td/text()')[0].extract()
     	pokemon['spDefense'] = sel.xpath('//tr[th/text() = "Sp. Def"]/td/text()')[0].extract()
     	pokemon['speed'] = sel.xpath('//tr[th/text() = "Speed"]/td/text()')[0].extract()
-    	pokemon['total'] = sel.xpath('//tr[th/text() = "Total"]/td/b/text()').extract()
+    	pokemon['total'] = sel.xpath('//tr[th/text() = "Total"]/td/b/text()')[0].extract()
     	return pokemon
